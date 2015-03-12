a853e762b1a0a42428edacece9b3c345fb37cc4a
Garrett Graham <ganderson7197@icloud.com>
2015-03-12 11:40:42 -0400

    changed the class on from jumbotron to heroImage to style the page a bit better. pretty disorganized here so trying to reign myself in. going to focus on styling the home page before i get to the javascript routing.

--- app/scripts/poses.js
+++ app/scripts/poses.js
@@ -3,21 +3,21 @@
 
 var poses = [
     {
-    englishTitle: "Downward-Facing Dog",
-    sanskritTitle: "Adho Mukha Svanasana",
-    benefit: "Build Focus",
-    image:"",
+    englishTitle: 'Downward-Facing Dog',
+    sanskritTitle: 'Adho Mukha Svanasana',
+    benefit: 'Build Focus',
+    image:'',
     },
     {
-    englishTitle: "Heron",
-    sanskritTitle: "Krounchasana",
-    benefit:"Calms the Mind",
-    image:"",
+    englishTitle: 'Heron',
+    sanskritTitle: 'Krounchasana',
+    benefit:'Calms the Mind',
+    image:'',
     },
     {
-    englishTitle: "Lion",
-    sanskritTitle: "Simhasana",
-    benefit: "Reduces Stress",
+    englishTitle: 'Lion',
+    sanskritTitle: 'Simhasana',
+    benefit: 'Reduces Stress',
     },    
 ]
 
