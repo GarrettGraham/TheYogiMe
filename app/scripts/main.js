d9674990e95c7c1b240714ed1e28850825984a1c
Garrett Graham <ganderson7197@icloud.com>
2015-03-12 13:47:06 -0400

    found a jquery function on stackoverflow that got the hero image the size i would like but the screen now has a horizontal scroll and i need to figure out how to eliminate the scroll. there is also whitespace on the left of the image and i would like to span the banner without any white space.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,35 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+//console.log('\'Allo \'Allo!');
+
+//hero banner
+$(document).ready(function () {
+var resizeHero = function()
+    { 
+        var hero = $("#banner"), window1 = $(window);
+            hero.css({
+                "width": window1.width(),
+                "height": window1.height(),
+                     });
+        
+        if(window1.width() < window1.height())
+        {
+            hero.find("img").css({
+                "width":window1.width(),
+                "height":"auto"
+            });
+        }
+        else
+        {
+            hero.find("img").css({
+                "height": window1.height(),
+                "width": "auto"
+            });
+        }
+    };
+    
+    resizeHero();
+    
+    $(window).reize(function(){
+        resizeHero();
+    });
+});
