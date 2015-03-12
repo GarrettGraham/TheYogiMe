e81f494ff5b568081cca98b2277846d15b357d72
Garrett Graham <ganderson7197@icloud.com>
2015-03-12 13:54:56 -0400

    commented out the jquery function and switching to css to style the hero image. having difficulty with calling it and setting the size to cover

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,34 +2,32 @@
 //console.log('\'Allo \'Allo!');
 
 //hero banner
-$(document).ready(function () {
-var resizeHero = function()
-    { 
-        var hero = $("#banner"), window1 = $(window);
-            hero.css({
-                "width": window1.width(),
-                "height": window1.height(),
-                     });
-        
-        if(window1.width() < window1.height())
-        {
-            hero.find("img").css({
-                "width":window1.width(),
-                "height":"auto"
-            });
-        }
-        else
-        {
-            hero.find("img").css({
-                "height": window1.height(),
-                "width": "auto"
-            });
-        }
-    };
-    
-    resizeHero();
-    
-    $(window).reize(function(){
-        resizeHero();
-    });
-});
+//$(document).ready(function () {
+//var resizeHero = function()
+//    { 
+//        var hero = $("#banner"), window1 = $(window);
+//            hero.css({
+//                "width": window1.width(),
+//                "height": window1.height(),
+//                     });
+//        
+//        if(window1.width() < window1.height())
+//        {
+//            hero.find("img").css({
+//                "width":window1.width(),
+//                "height":"auto"
+//            });
+//        }
+//        else
+//        {
+//            hero.find("img").css({
+//                "height": window1.height(),
+//                "width": "auto"
+//            });
+//        }
+//    };
+//    resizeHero();
+//    $(window).reize(function(){
+//        resizeHero();
+//    });
+//});
