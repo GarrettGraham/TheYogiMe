761d664ff68edb4620f5c5987dd1b2ef3104b5eb
Garrett Graham <ganderson7197@icloud.com>
2015-03-12 19:32:00 -0400

    writing the basics for the routing but need to match up the index file names with those designated within the angular module

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,9 @@
 /* jshint devel:true */
 //console.log('\'Allo \'Allo!');
 
-//hero banner
-//$(document).ready(function () {
-//var resizeHero = function()
-//    { 
-//        var hero = $("#banner"), window1 = $(window);
-//            hero.css({
-//                "width": window1.width(),
-//                "height": window1.height(),
-//                     });
-//        
-//        if(window1.width() < window1.height())
-//        {
-//            hero.find("img").css({
-//                "width":window1.width(),
-//                "height":"auto"
-//            });
-//        }
-//        else
-//        {
-//            hero.find("img").css({
-//                "height": window1.height(),
-//                "width": "auto"
-//            });
-//        }
-//    };
-//    resizeHero();
-//    $(window).reize(function(){
-//        resizeHero();
-//    });
-//});
+angular.module('theYogiMe', [
+    'ui.router'
+])
+.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+
+}]);
