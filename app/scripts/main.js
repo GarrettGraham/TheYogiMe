ee0f351daad59a538715bcdbdb646af2b8a5b86e
Garrett Graham <ganderson7197@icloud.com>
2015-03-13 09:46:51 -0400

    added angular and angular-ui-router into the dependencies on the bower.json file. got a good starting point for the routes on the page. moved the nav into its own html file and it now will show up in any view. moved the main div of the content into the main.html file and it also shows up with the ui div on the index.html file. pasted a div into the meditate.html file and fixed the router function thanks to the help of logan.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,5 +5,20 @@ angular.module('theYogiMe', [
     'ui.router'
 ])
 .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'main.html'
+      })
+      .state('yoga', {
+        url:'/yoga',
+        templateUrl: 'app/yoga.html'
+      })
+      .state('meditate', {
+        url:'/meditate',
+        templateUrl: 'app/meditate.html'
+      })
 
-}]);
+}])
