6b66a4e533e4daa6b2de53799d2eaf6ab84a4a78
Garrett Graham <ganderson7197@icloud.com>
2015-03-20 12:57:57 -0400

    ran gulp build and fixed some things the lint threw to me but it is saying the angular is not defined but everything is rendering to i am not sure what this is all about. working toward getting the login and log out feature

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,39 +1,38 @@
 /* jshint devel:true */
 //console.log('\'Allo \'Allo!');
-
+'use strict'
 
 var app = angular.module('theYogiMe', [
-    'ui.router', 
+    'ui.router',
     'uiGmapgoogle-maps'
 ])
-.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+app.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
     $urlRouterProvider.otherwise('/');
-    
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'templates/main.html',
-        controller: 'mainCtrl as main'
-      })
-      .state('yoga', {
-        url:'/yoga',
-        templateUrl: 'templates/yoga.html',
-        controller: 'yogaCtrl as yoga'
-      })
-      .state('poses', {
-        url:'/poses',
-        templateUrl: 'templates/poses.html',
-        controller: 'posesCtrl as poses'
-    })
-      .state('meditate', {
-        url:'/meditate',
-        templateUrl: 'templates/meditate.html',
-        controller: 'meditateCtrl as meditate'
-    })
-      .state('studioMap', {
-        url:'/studioMap',
-        templateUrl: 'templates/studioMap.html',
-        controller: 'mapCtrl as studioMap'
-    })
-}])
 
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'templates/main.html',
+            controller: 'mainCtrl as main'
+        })
+        .state('yoga', {
+            url: '/yoga',
+            templateUrl: 'templates/yoga.html',
+            controller: 'yogaCtrl as yoga'
+        })
+        .state('poses', {
+            url: '/poses',
+            templateUrl: 'templates/poses.html',
+            controller: 'posesCtrl as poses'
+        })
+        .state('meditate', {
+            url: '/meditate',
+            templateUrl: 'templates/meditate.html',
+            controller: 'meditateCtrl as meditate'
+        })
+        .state('studioMap', {
+            url: '/studioMap',
+            templateUrl: 'templates/studioMap.html',
+            controller: 'mapCtrl as studioMap'
+        });
+}]);
