6cb226bcdd6a4b56a1418c4d58854c93d35f3629
Garrett Graham <ganderson7197@icloud.com>
2015-04-01 18:08:06 -0400

    created a console.log test to play with this and scope on the console but now going to work on the playlist feature for the meditation page.

--- app/scripts/controllers/meditateCtrl.js
+++ app/scripts/controllers/meditateCtrl.js
@@ -1,6 +1,6 @@
 'use strict'
 
 angular.module('theYogiMe')
-.controller('meditateCtrl', function() {
-//    console.log('hello')
-});
+    .controller('meditateCtrl', function ($scope) {
+        //        console.log($scope)
+    });
