c8f013ca22257678f41e12f037c06a0907aff1ff
Garrett Graham <ganderson7197@icloud.com>
2015-04-08 15:02:20 -0400

    renamed the templateUrls in the main.js and changed the header for the ng include so the nav shows up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,46 +16,46 @@ app.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider
 	$stateProvider
 		.state('home', {
 			url: '/',
-			templateUrl: 'templates/main.html',
+			templateUrl: '/main.html',
 			controller: 'mainCtrl',
 			controllerAs: 'main'
 		})
 		.state('yoga', {
 			url: '/yoga',
-			templateUrl: 'templates/yoga.html',
+			templateUrl: '/yoga.html',
 			controller: 'yogaCtrl',
 			controllerAs: 'yoga'
 		})
 		.state('poses', {
 			url: '/poses',
-			templateUrl: 'templates/poses.html',
+			templateUrl: '/poses.html',
 			controller: 'posesCtrl',
 			controllerAs: 'poses'
 		})
 		// below is the route to the yoga videos
 		.state('poses.chairYoga', {
 			url: '/chairYoga',
-			templateUrl: 'templates/chairYoga.html'
+			templateUrl: '/chairYoga.html'
 		})
 		.state('poses.downDog', {
 			url: '/downDog',
-			templateUrl: 'templates/downDog.html'
+			templateUrl: '/downDog.html'
 		})
 		.state('poses.warriorII', {
 			url: '/warriorII',
-			templateUrl: 'templates/warriorII.html'
+			templateUrl: '/warriorII.html'
 		})
 		//meditation page with audio
 		.state('meditate', {
 			url: '/meditate',
-			templateUrl: 'templates/meditate.html',
+			templateUrl: '/meditate.html',
 			controller: 'meditateCtrl',
 			controllerAs: 'meditate'
 		})
 		//schedule a private lesson through email form
 		.state('scheduleLesson', {
 			url: '/scheduleLesson',
-			templateUrl: 'templates/scheduleLesson.html',
+			templateUrl: '/scheduleLesson.html',
 			controller: 'lessonCtrl',
 			controllerAs: 'scheduleLesson'
 		});
