4efd07418417f7b6ef994520a6242f22a9f77b8c
Garrett Graham <ganderson7197@icloud.com>
2015-04-14 10:22:35 -0400

    working on getting mandrill to send the email form I created through mandrill since the fireform is not working as I would like it.

--- app/scripts/controllers/lessonCtrl.js
+++ app/scripts/controllers/lessonCtrl.js
@@ -2,7 +2,54 @@
 
 angular.module('theYogiMe')
 	.controller('lessonCtrl', function () {
-		$('#submitEmail').on('click', function () {
-			alert('Your email was sent successfully');
-		});
-	});
+
+			jQuery(function ($) {
+				$("#contactForm").submit(function () {
+
+					//		$('#submitEmail').on('click', function () {
+					//			alert('Your email was sent successfully');
+				});
+			});
+
+			//jQuery(function($)  
+			//{
+			//    $("#contact_form").submit(function()
+			//    {
+			//        var email = $("#email").val(); // get email field value
+			//        var name = $("#name").val(); // get name field value
+			//        var msg = $("#msg").val(); // get message field value
+			//        $.ajax(
+			//        {
+			//            type: "POST",
+			//            url: "https://mandrillapp.com/api/1.0/messages/send.json",
+			//            data: {
+			//                'key': 'sVRLFidC1A7K56TuUkyUQg',
+			//                'message': {
+			//                    'from_email': email,
+			//                    'from_name': name,
+			//                    'headers': {
+			//                        'Reply-To': email
+			//                    },
+			//                    'subject': 'Website Contact Form Submission',
+			//                    'text': msg,
+			//                    'to': [
+			//                    {
+			//                        'email': 'riyadh@bscheme.com',
+			//                        'name': 'Riyadh Al Nur',
+			//                        'type': 'to'
+			//                    }]
+			//                }
+			//            }
+			//        })
+			//        .done(function(response) {
+			//            alert('Your message has been sent. Thank you!'); // show success message
+			//            $("#name").val(''); // reset field after successful submission
+			//            $("#email").val(''); // reset field after successful submission
+			//            $("#msg").val(''); // reset field after successful submission
+			//        })
+			//        .fail(function(response) {
+			//            alert('Error sending message.');
+			//        });
+			//        return false; // prevent page refresh
+			//    });
+			//});
