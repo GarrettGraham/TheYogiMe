2d5989c0b88f1888aa6fa0377f7af0ceb8126ea9
Garrett Graham <ganderson7197@icloud.com>
2015-04-15 15:56:29 -0400

    successully built and deployed to firebase and the email is sending successfully. now starting to clean up the files in my project, removing the commented out code but havent added that yet. need to then merge the release branch into master and deploy

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -1,6 +1,4 @@
 'use strict';
 
 angular.module('theYogiMe')
-	.controller('mainCtrl', function () {
-		//    here is where you input the code for the home page to show active vs non active? is this where i may change the background color to that green??
-	});
+	.controller('mainCtrl', function () {});
