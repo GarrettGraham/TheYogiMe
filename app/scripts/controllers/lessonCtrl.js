11c927b1f6804a76e8e972a8665a0bc941ce4f9a
Garrett Graham <ganderson7197@icloud.com>
2015-04-15 11:40:57 -0400

    input the template content with example name and example content keys and values and uncommented the first global merge variable to test if it works

--- app/scripts/controllers/lessonCtrl.js
+++ app/scripts/controllers/lessonCtrl.js
@@ -5,45 +5,47 @@ angular.module('theYogiMe')
 		//		var self = this;
 		jQuery(function ($) {
 			$('#contactForm').submit(function () {
-					var name = $('#clientName').val(); //get first name field value
-					var classSelection = $('#classSelection').val(); //get class selection field value
-					var clientGoals = $('#clientGoals').val(); //get client goals selection field value
-					var lessonTime = $('#lessonTime').val(); //get lesson time selection field value 
-					var clientInfo = $('#clientInfo').val(); // get client info field value
-					var firstName = $('#firstName').val(); //get client first name field value
-					var lastName = $('#lastName').val(); // get client last name field value
-					var clientEmail = $('#clientEmail').val(); //get client email field value
-					var phoneNumber = $('#phoneNumber').val(); //get client phone number field value
+				var name = $('#clientName').val(); //get first name field value
+				var classSelection = $('#classSelection').val(); //get class selection field value
+				var clientGoals = $('#clientGoals').val(); //get client goals selection field value
+				var lessonTime = $('#lessonTime').val(); //get lesson time selection field value 
+				var clientInfo = $('#clientInfo').val(); // get client info field value
+				var firstName = $('#firstName').val(); //get client first name field value
+				var lastName = $('#lastName').val(); // get client last name field value
+				var clientEmail = $('#clientEmail').val(); //get client email field value
+				var phoneNumber = $('#phoneNumber').val(); //get client phone number field value
 
-					//				$http({
-					$.ajax({
-							method: "POST",
-							url: "https://mandrillapp.com/api/1.0/messages/send.json",
-							data: {
-								'key': 'a2o-Xz8u1Lrb75Z2NlscsQ',
-								'template_name': 'the-yogi-me',
-								'template_content': [
-									{
+				//				$http({
+				$.ajax({
+						method: "POST",
+						url: "https://mandrillapp.com/api/1.0/messages/send.json",
+						data: {
+							'key': 'a2o-Xz8u1Lrb75Z2NlscsQ',
+							'template_name': 'the-yogi-me',
+							'template_content': [
+								{
+									'name': 'example name',
+									'content': 'example content'
 									}
 								],
-															'message': {
-														'from_email': clientEmail,
-														'from_name': name,
-														'headers': {
-															'Reply-To': clientEmail
-														},
-														'subject': 'Client Lead Contact Form Submission',
-														//								'text': 'clietnGoals' && 'clientInfo',
-														'to': [
-															{
-																'email': 'ganderson7197@icloud.com',
-																'name': 'Garrett Graham',
-																'type': 'to'
+							'message': {
+								'from_email': clientEmail,
+								'from_name': name,
+								'headers': {
+									'Reply-To': clientEmail
+								},
+								'subject': 'Client Lead Contact Form Submission',
+								//								'text': 'clietnGoals' && 'clientInfo',
+								'to': [
+									{
+										'email': 'ganderson7197@icloud.com',
+										'name': 'Garrett Graham',
+										'type': 'to'
 																					}],
-														'global_merge_vars': [
-																{
-																	'name': 'name',
-																	'content': $scope.clientName
+								'global_merge_vars': [
+									{
+										'name': 'name',
+										'content': $scope.clientName
 																												},
 								//									{
 								//										'name': 'classSelection',
@@ -78,28 +80,28 @@ angular.module('theYogiMe')
 								//										'content': this.phoneNumber
 								//												},
 								]
+							}
 						}
-					}
-				})
-				.success(function (response) {
-					alert('Your messsage has been sent. Thank you!'); //show success message
-					var name = $('#clientName').val(''); //reset first name field value
-					var classSelection = $('#classSelection').val(''); //reset class selection field value
-					var clientGoals = $('#clientGoals').val(''); //reset client goals selection field value
-					var lessonTime = $('#lessonTime').val(''); //reset lesson time selection field value 
-					var clientInfo = $('#clientInfo').val(''); // reset client info field value
-					var firstName = $('#firstName').val(''); // reset client first name field value
-					var lastName = $('#lastName').val(''); // reset client last name field value
-					var clientEmail = $('#clientEmail').val(''); // reset client email field value
-					var phoneNumber = $('#phoneNumber').val(''); // reset client phone number field value
-				})
-				.error(function (response) {
-					alert('Error sending message.');
-				});
+					})
+					.success(function (response) {
+						alert('Your messsage has been sent. Thank you!'); //show success message
+						var name = $('#clientName').val(''); //reset first name field value
+						var classSelection = $('#classSelection').val(''); //reset class selection field value
+						var clientGoals = $('#clientGoals').val(''); //reset client goals selection field value
+						var lessonTime = $('#lessonTime').val(''); //reset lesson time selection field value 
+						var clientInfo = $('#clientInfo').val(''); // reset client info field value
+						var firstName = $('#firstName').val(''); // reset client first name field value
+						var lastName = $('#lastName').val(''); // reset client last name field value
+						var clientEmail = $('#clientEmail').val(''); // reset client email field value
+						var phoneNumber = $('#phoneNumber').val(''); // reset client phone number field value
+					})
+					.error(function (response) {
+						alert('Error sending message.');
+					});
 
 
-			//		$('#submitEmail').on('click', function () {
-			//			alert('Your email was sent successfully');
+				//		$('#submitEmail').on('click', function () {
+				//			alert('Your email was sent successfully');
+			});
 		});
 	});
-});
