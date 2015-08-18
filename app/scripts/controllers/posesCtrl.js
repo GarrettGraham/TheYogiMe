eb9557f76801b322f6e7fc3c4c9ae704a6433704
Garrett Graham <ganderson7197@icloud.com>
2015-08-18 16:23:31 -0400

    changed the ng repeate so thebinding can perform.

--- app/scripts/controllers/posesCtrl.js
+++ app/scripts/controllers/posesCtrl.js
@@ -4,10 +4,18 @@
 angular.module('theYogiMe')
 	.controller('posesCtrl', function($http, $firebaseArray){
 	var self = this;
-// persist/save data to firebase using angularfire
-var ref = new Firebase('http://oc-run.firebaseio.com/poses/' + 'comments');
+	//save data to firebase using angularfire
+$http.get('https://theyogime.firebaseio.com/poses/.json')
+.then(function(response){
+	self.comment = response.data;
+	// console.log(self.comment);
+});
+
+// persist data below the form
+var ref = new Firebase('http://theyogime.firebaseio.com/poses/comments');
 // ng model comment info to populate below the form
 	self.comments = $firebaseArray(ref);
+
 	self.addComment = function(){
 		self.comments.$add ({
 			name: self.newCommentName,
