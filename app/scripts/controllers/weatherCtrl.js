aab8b4bf504df9dff4c8095e50e0e387124ae393
Garrett Graham <ganderson7197@icloud.com>
2015-12-07 15:40:16 -0500

    completed the factory service to retrieve weather data from the yahoo api

--- app/scripts/controllers/weatherCtrl.js
+++ app/scripts/controllers/weatherCtrl.js
@@ -2,8 +2,21 @@
   'use strict';
   angular.module('theYogiMe', []);
   app.factory('yogiWeather', ['$http', '$q', function($http, $q){
+    function getWeather (zip){
+      var deferred = $q.defer();
+      $http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.forecast%20WHERE%20location%3D%22' + zip + '%22&format=json&diagnostics=true&callback=')
+      .success(function(data){
+        deferred.resolve(data.query.result.channel);
+      })
+      .error(function(err){
+        console.log('Error retrieving markets');
+        deferred.reject(err);
+      });
+      return deferred.promise;
+    }
     return{
-      
+      getWeather: getWeather
     };
   }]);
+  
 })();
