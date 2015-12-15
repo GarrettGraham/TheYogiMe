415ae0b250eaabc3fd60cadef82e48a711cd5517
Garrett Graham <ganderson7197@icloud.com>
2015-12-15 18:06:12 -0500

    playing around with to try and get the data from forecast. was getting a get error message because of a connection error that was not allowing for communication with the API. I was playing around with where the variables of lat lon and apikey should be and kept getting the same error thrown. first step is to get the data and api communication then bind the controller with the view to populate the data.

--- app/scripts/controllers/weatherCtrl.js
+++ app/scripts/controllers/weatherCtrl.js
@@ -3,15 +3,14 @@
 
   angular.module('theYogiMe')
   .factory('yogiWeather', ['$http', '$q', function($http, $q){
-    var apiKey = '96e767315d45d96b025baf3e22eced01',
-    lat = '28.538335',
-    lon = '-81.379236',
-    cachedForecast; //cache data from server request in order to store weather data from each query
+    var apiKey = '31bd9688a15a6e5353428ad1923d0f3a';
+      // cachedForecast; //cache data from server request in order to store weather data from each query
 
     function getWeather (zip){
       var deferred = $q.defer(),
-      zip = lat + ',' + lon;
-      $http.get('https://api.forecast.io/forecast/' + apiKey + '/' + zip + '/?callback=JSON_CALLBACK' )
+      lat = '28.538335',
+      lon = '-81.379236';
+      $http.get('http://api.forecast.io/forecast/' + apiKey + '/' + lat + ',' + lon)
       .success(function(data){
         deferred.resolve(data.query.results.channel);
       })
@@ -28,17 +27,17 @@
 
 angular.module('theYogiMe')
 .controller('weatherCtrl', ['$scope', 'yogiWeather', function($scope, yogiWeather){
-  function fetchWeather(zip) {
-    yogiWeather.getWeather(zip).then(function(data){
-      $scope.place = data;
-    });
-  }
-  fetchWeather('32750');
-
-  $scope.findWeather = function(zip) {
-    $scope.place = '';
-    fetchWeather(zip);
-  };
+  // function fetchWeather(zip) {
+  //   yogiWeather.getWeather(zip).then(function(data){
+  //     $scope.place = data;
+  //   });
+  // }
+  // fetchWeather('32750');
+  //
+  // $scope.findWeather = function(zip) {
+  //   $scope.place = '';
+  //   fetchWeather(zip);
+  // };
 }]);
 // want to create a script that changes the background image of the page depending on the weather of the day. if it is sunny, display a animated background of a sunny day at the park. if it is cloudy display an appropriate animated image, if it is rainy/ stormy display an appropriate image, and if it is dusk show an appropriate image and if it is evening, show an appropriate image.
 })();
