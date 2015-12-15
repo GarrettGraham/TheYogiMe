65087d8da56f6a8e7319c164ca4a10252e63a063
Garrett Graham <ganderson7197@icloud.com>
2015-12-15 13:25:16 -0500

    working on changing current code using yahoo weather api to using the forcast.io api to get the weather data. I was initially not able to gain access to the server because the url and syntax was off and the binding with the html broke so only showed curly braces with object interation within html. now the binding is happening but the data is not coming through because the data is not able to load, getting an access-control-allow-origin header is present on the requested resource message

--- app/scripts/controllers/weatherCtrl.js
+++ app/scripts/controllers/weatherCtrl.js
@@ -3,9 +3,15 @@
 
   angular.module('theYogiMe')
   .factory('yogiWeather', ['$http', '$q', function($http, $q){
+    var apiKey = '96e767315d45d96b025baf3e22eced01',
+    lat = '28.538335',
+    lon = '-81.379236',
+    cachedForecast; //cache data from server request in order to store weather data from each query
+
     function getWeather (zip){
-      var deferred = $q.defer();
-      $http.get('https://query.yahooapis.com/v1/public/yql?q=desc20%weather.forecast' + zip + '&format=json&diagnostics=true&callback=')
+      var deferred = $q.defer(),
+      zip = lat + ',' + lon;
+      $http.get('https://api.forecast.io/forecast/' + apiKey + '/' + zip + '/?callback=JSON_CALLBACK' )
       .success(function(data){
         deferred.resolve(data.query.results.channel);
       })
